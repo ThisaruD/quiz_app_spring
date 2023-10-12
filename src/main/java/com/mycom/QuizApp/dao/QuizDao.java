4d7b16e879cfdbb8c5acb7cded0715b887a37daa
ThisaruD <thisaru.18@itfac.mrt.ac.lk>
2023-10-12 14:41:36 +0530

    first commit

---
+++ src/main/java/com/mycom/QuizApp/dao/QuizDao.java
@@ -0,0 +1,9 @@
+package com.mycom.QuizApp.dao;
+
+import com.mycom.QuizApp.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuizDao extends JpaRepository<Quiz,Integer> {
+}
